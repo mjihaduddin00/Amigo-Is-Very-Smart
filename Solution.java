32da816e2f89885597d7469b8c5fd58a97036c44
Mohammad Jihad Uddin <mjihaduddin00@outlook.com>
2021-02-26 22:23:19 -0600

    Main File
    I am committing the main file that the application is comprised of (Java File).

---
+++ Solution.java
@@ -0,0 +1,10 @@
+/*                                                   
+Amigo is very smart                                                  
+                                                  
+*/                                                  
+                                                  
+public class Solution {                                                  
+    public static void main(String[] args) {                                                  
+        System.out.println("Amigo is very smart");                                                  
+    }                                                  
+}
